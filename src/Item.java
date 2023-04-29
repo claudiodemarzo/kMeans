14418f5a5a4db0955a457740ff1fe52784864633
Claudio De Marzo <claudiodemarzo.62@gmail.com>
2023-04-29 15:54:50 +0200

    Aggiunto javadoc per Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,28 +1,73 @@
+/**
+ * Item.java
+ * <p>
+ * Classe che modella una coppia generica attrivuto-valore
+ *
+ * @author Claudio De Marzo
+ */
 public abstract class Item {
-
+    /**
+     * Attributo dell'item
+     */
     private Attribute attribute;
+
+    /**
+     * Valore dell'item
+     */
     private Object value;
+
+    /**
+     * Costruttore della classe Item
+     * @param attribute attributo dell'item
+     * @param value valore dell'item
+     */
 
     Item(Attribute attribute, Object value) {
         this.attribute = attribute;
         this.value = value;
     }
 
+    /**
+     * Restituisce l'attributo dell'item
+     * @return l'attributo dell'item
+     */
+
     Attribute getAttribute() {
         return attribute;
     }
 
+    /**
+     * Restituisce il valore dell'item
+     * @return il valore dell'item
+     */
     Object getValue() {
         return value;
     }
+
+    /**
+     * Restituisce una rappresentazione testuale dell'item
+     * @return una rappresentazione testuale dell'item, che coincide con il valore dell'item
+     */
 
     @Override
     public String toString() {
         return value.toString();
     }
 
+    /**
+     * Restituisce la distanza tra due item
+     * @param a l'oggetto rispetto al quale calcolare la distanza
+     * @return la distanza tra i due item
+     */
+
     abstract double distance(Object a);
 
+
+    /**
+     * Aggiorna il valore dell'item, calcolandolo come il prototipo dell'attributo rispetto al clusteredData
+     * @param data il dataset
+     * @param clusteredData l'insieme degli indici delle transazioni appartenenti al cluster in considerazione
+     */
     void update(Data data, ArraySet clusteredData) {
         value = data.computePrototype(clusteredData, attribute);
     }
