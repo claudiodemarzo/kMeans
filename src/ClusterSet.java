bbc452d0b2cff259452e2cd8751981ec26eaa1fc
Claudio De Marzo <claudiodemarzo.62@gmail.com>
2023-04-29 16:14:09 +0200

    Aggiunto javadoc per Cluster.java

--- src/ClusterSet.java
+++ src/ClusterSet.java
@@ -1,19 +1,56 @@
+/**
+ * ClusterSet.java
+ * <p>
+ * Classe che modella un insieme di Cluster
+ *
+ * @see Cluster
+ * @author Claudio De Marzo
+ */
 public class ClusterSet {
+
+    /**
+     * Array di Cluster
+     */
     private Cluster[] C;
+
+    /**
+     * Indice dell'ultima posizione di C libero
+     */
     private int i = 0;
+
+    /**
+     * Costruttore della classe ClusterSet
+     * @param k il numero di cluster contenuti nel Set
+     */
 
     ClusterSet(int k) {
         C = new Cluster[k];
     }
+
+    /**
+     * Aggiunge un cluster all'insieme
+     * @param c il Cluster da aggiungere
+     */
 
     void add(Cluster c) {
         C[i] = c;
         i++;
     }
 
+    /**
+     * Restituisce un cluster dal Set dato l'indice
+     * @param i l'indice del cluster da restituire
+     * @return il cluster richiesto
+     */
+
     Cluster get(int i) {
         return C[i];
     }
+
+    /**
+     * Sceglie i centroidi dal dataset e inizializza i cluster nel ClusterSet
+     * @param data il dataset
+     */
 
     void initializeCentroids(Data data) {
         int[] centroidIndexes = data.sampling(C.length);
@@ -22,6 +59,12 @@ public class ClusterSet {
             add(new Cluster(centroidI));
         }
     }
+
+    /**
+     * Restituisce il cluster più vicino ad una tupla
+     * @param tuple la tupla
+     * @return il cluster più vicino
+     */
 
     Cluster nearestCluster(Tuple tuple) {
         Cluster nearest = null;
@@ -35,17 +78,32 @@ public class ClusterSet {
         return nearest;
     }
 
+    /**
+     * Restituisce il cluster che contiene una certa transazione, dato l'indice di questa
+     * @param id l'indice della transazione
+     * @return il cluster che contiene la transazione, o null se non è contenuta in nessun cluster
+     */
     Cluster currentCluster(int id) {
         for (int i = 0; i < this.i; i++)
             if (C[i].contain(id)) return C[i];
         return null;
     }
 
+    /**
+     * Aggiorna i centroidi dei cluster
+     * @param data il dataset
+     */
+
     void updateCentroids(Data data) {
         for (int i = 0; i < this.i; i++) {
             C[i].computeCentroid(data);
         }
     }
+
+    /**
+     * Restituisce una stringa fatta da ciascun centroide dell’insieme dei cluster.
+     * @return la stringa richiesta
+     */
 
     @Override
     public String toString() {
@@ -56,6 +114,11 @@ public class ClusterSet {
         return s;
     }
 
+    /**
+     * Restituisce una stringa che descriva lo stato di ciascun cluster in C.
+     * @param data il dataset
+     * @return la stringa richiesta
+     */
     public String toString(Data data) {
         String str = "";
         for (int i = 0; i < this.i; i++) {
